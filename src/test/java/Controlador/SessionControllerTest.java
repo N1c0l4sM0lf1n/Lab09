3ffce4e284f8cd0c797527677c2a4849651ad0b0
dci <n.molfinqueo01@ufromail.cl>
2026-06-11 09:46:02 -0400

    SessionControllerTest

--- src/test/java/Controlador/SessionControllerTest.java
+++ src/test/java/Controlador/SessionControllerTest.java
@@ -1,4 +1,81 @@
 package Controlador;
 
-public class SessionControllerTest {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SessionControllerTest {
+
+    @Test
+    void loginCorrecto() {
+
+        SessionController session =
+                new SessionController();
+
+        session.registrarUsuario(
+                "nico",
+                "1234",
+                "Nicolas"
+        );
+
+        assertTrue(
+                session.iniciarSesion(
+                        "nico",
+                        "1234"
+                )
+        );
+    }
+
+    @Test
+    void loginUsuarioInexistente() {
+
+        SessionController session =
+                new SessionController();
+
+        assertFalse(
+                session.iniciarSesion(
+                        "fantasma",
+                        "1234"
+                )
+        );
+    }
+
+    @Test
+    void loginUsernameNull() {
+
+        SessionController session =
+                new SessionController();
+
+        session.registrarUsuario(
+                "nico",
+                "1234",
+                "Nicolas"
+        );
+
+        assertFalse(
+                session.iniciarSesion(
+                        null,
+                        "1234"
+                )
+        );
+    }
+
+    @Test
+    void cerrarSesionDebeEliminarUsuario() {
+
+        SessionController session =
+                new SessionController();
+
+        session.registrarUsuario(
+                "nico",
+                "1234",
+                "Nicolas"
+        );
+
+        session.cerrarSesion();
+
+        assertNull(
+                session.getUsuario()
+        );
+    }
 }
