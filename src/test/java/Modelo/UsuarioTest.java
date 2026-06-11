b0ceab3494ac35d8021d1b3d505a14077bb6116e
dci <n.molfinqueo01@ufromail.cl>
2026-06-11 09:40:22 -0400

    UsuarioTest

--- src/test/java/Modelo/UsuarioTest.java
+++ src/test/java/Modelo/UsuarioTest.java
@@ -1,4 +1,62 @@
 package Modelo;
 
-public class UsuarioTest {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UsuarioTest {
+
+    @Test
+    void credencialesCorrectas() {
+
+        Usuario usuario =
+                new Usuario(
+                        "nico",
+                        "1234",
+                        "Nicolas"
+                );
+
+        assertTrue(
+                usuario.validarCredenciales(
+                        "nico",
+                        "1234"
+                )
+        );
+    }
+
+    @Test
+    void passwordIncorrecta() {
+
+        Usuario usuario =
+                new Usuario(
+                        "nico",
+                        "1234",
+                        "Nicolas"
+                );
+
+        assertFalse(
+                usuario.validarCredenciales(
+                        "nico",
+                        "xxxx"
+                )
+        );
+    }
+
+    @Test
+    void usernameIncorrecto() {
+
+        Usuario usuario =
+                new Usuario(
+                        "nico",
+                        "1234",
+                        "Nicolas"
+                );
+
+        assertFalse(
+                usuario.validarCredenciales(
+                        "otro",
+                        "1234"
+                )
+        );
+    }
 }
